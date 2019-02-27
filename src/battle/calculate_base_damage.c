cbbc346a1000cb0c5c11c150f70cb54db5940fa4
LOuroboros <lunosouroboros@gmail.com>
2019-02-27 03:50:36 -0300

    Updated the Light Ball's effect.

--- src/battle/calculate_base_damage.c
+++ src/battle/calculate_base_damage.c
@@ -189,7 +189,7 @@ s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *de
     if (defenderHoldEffect == HOLD_EFFECT_DEEP_SEA_SCALE && defender->species == SPECIES_CLAMPERL)
         spDefense *= 2;
     if (attackerHoldEffect == HOLD_EFFECT_LIGHT_BALL && attacker->species == SPECIES_PIKACHU)
-        spAttack *= 2;
+        spAttack *= 2, attack *= 2;
     if (defenderHoldEffect == HOLD_EFFECT_METAL_POWDER && defender->species == SPECIES_DITTO)
         defense *= 2;
     if (attackerHoldEffect == HOLD_EFFECT_THICK_CLUB && (attacker->species == SPECIES_CUBONE || attacker->species == SPECIES_MAROWAK))
